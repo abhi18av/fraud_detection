f6baf7ca2446c85ed4aaf7946d21479e0c494244
Abhinav Sharma <abhi18av@gmail.com>
2020-03-14 15:28:28 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The final project for IIIT-B PGDDS certification',
+    author='Abhinav Sharma ',
+    license='MIT',
+)
